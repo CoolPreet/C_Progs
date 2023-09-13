0a7e473c528c3cffaf277ca7822d667382f9be36
Preet Sahoo <preeti.sahoo@honeywell.com>
2023-09-13 12:22:35 +0530

    Test commit

---
+++ LKF/000_Template.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int count = 0;
+    
+    ++count;
+    ++count;
+    ++count;
+    ++count;
+    ++count;
+    ++count;
+    ++count;
+    ++count;
+    ++count;
+    ++count;
+    ++count;
+    ++count;
+    ++count;
+    ++count;
+    ++count;
+    ++count;
+    ++count;
+    ++count;
+    ++count;
+    ++count;
+    ++count;
+    ++count;
+    ++count;
+    ++count;
+    ++count;
+    ++count;
+    ++count;
+    ++count;
+    ++count;
+    ++count;
+    ++count;
+    ++count;
+    ++count;
+    ++count;
+    ++count;
+
+    puts("Jay Laddoo Gopal\n");
+
+    return EXIT_SUCCESS;
+}
